1d657445bd21a9e53da4853163d2a5a21534132b
runabrandes <>
2023-12-13 14:15:25 +0000

    Added multiplication game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-        } else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else {
             alert(`Unknown game type: ${gameType}`);
             throw `Unknown game type: ${gameType}. Aborting!`;
         }
@@ -70,6 +72,8 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return[operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -105,10 +109,14 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-function displaySubtractQuestion() { 
+function displaySubtractQuestion(operand1, operand2) { 
 
 }
 
-function displayMultiplyQuestion() { 
+function displayMultiplyQuestion(operand1, operand2) { 
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
